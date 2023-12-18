e35b31f70082cd59c408da50927317982c69912e
Kayfah191 <153527802+Kayfah191@users.noreply.github.com>
2023-12-18 12:42:33 +0100

    hej

---
+++ JavaApplication1/src/MiB.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Kayhan
+ */
+public class MiB {
+    
+    asdfa
+            sdfas
+            dfa
+                    sdf
+                    as
+                            df
+                            asd
+                                    fa
+                                    sdf
+                                            a
+                                            sdf
+                                                    a
+                                                    sdf
+                                                            asdf
+                                                            
+}
