ed6cdbc1e3bf3d706db8e907dc463ff4cdc306f0
ad0mmy1 <adam040804@gmail.com>
2023-12-20 13:53:19 +0100

    Ändra info alien

--- JavaApplication1/src/AndraAliens_Admin.java
+++ JavaApplication1/src/AndraAliens_Admin.java
@@ -108,7 +108,11 @@ public class AndraAliens_Admin extends javax.swing.JFrame {
     }//GEN-LAST:event_jButton4ActionPerformed
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
-        // TODO add your handling code here:
+     AndraAliens AndraAlien= new AndraAliens();
+        AndraAlien.show(); //Ändra Nya Aliens
+        
+        dispose();  
+       // TODO add your handling code here:
     }//GEN-LAST:event_jButton2ActionPerformed
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
